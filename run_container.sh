49462777fd3d6834d49eef603d342f2a9ddedf07
Arjun <36100411+arjvn@users.noreply.github.com>
2024-04-28 02:35:44 +0100

    database redownload warning

--- run_container.sh
+++ run_container.sh
@@ -15,22 +15,25 @@ xhost +
 GDRIVE_URL="https://drive.google.com/uc?id=1WgyOXzUa_zOHbtz_e-dZDVLymAqkJqlQ"
 DATASET_NAME="avocado_dataset.zip"
 TARGET_DIR="$DIR/test_data"
+CHECK_TEST_SCENE_DIR="$DIR/test_data/avocado_translate_1/JPEGImages" # Ensuring full path is correct
 
 # Run Docker container and execute the script
 docker run --gpus all --env NVIDIA_DISABLE_REQUIRE=1 -it --network=host --name foundationpose \
 --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -v $DIR:$DIR -v /home:/home -v /mnt:/mnt \
 -v /tmp/.X11-unix:/tmp/.X11-unix -v /tmp:/tmp --ipc=host -e DISPLAY=${DISPLAY} -e GIT_INDEX_FILE \
-foundationpose:latest bash -c "
-echo 'Requested test scene directory: ${TEST_SCENE_DIR}';
-if [ '${TEST_SCENE_DIR}' == 'test_data/avocado_translate_1' ]; then
-    echo 'Required test scene directory is avocado_translate_1, checking dataset...';
-    mkdir -p ${TARGET_DIR};
-    echo 'Downloading and preparing the dataset...';
-    pip install gdown;
-    gdown ${GDRIVE_URL} -O ${TARGET_DIR}/${DATASET_NAME} &&\
-    unzip -q ${TARGET_DIR}/${DATASET_NAME} -d ${TARGET_DIR} &&\
-     rm ${TARGET_DIR}/${DATASET_NAME};
-fi;
+foundationpose:latest bash -c "\
+echo 'Requested test scene directory: ${TEST_SCENE_DIR}'; \
+if [ '${TEST_SCENE_DIR}' == 'test_data/avocado_translate_1' ]; then \
+    if [ -z \"\$(ls -A $CHECK_TEST_SCENE_DIR)\" ]; then \
+        echo 'Required test scene directory is avocado_translate_1, checking dataset...'; \
+        mkdir -p ${TARGET_DIR}; \
+        echo 'Downloading and preparing the dataset...'; \
+        pip install gdown; \
+        gdown ${GDRIVE_URL} -O ${TARGET_DIR}/${DATASET_NAME} && \
+        unzip -q ${TARGET_DIR}/${DATASET_NAME} -d ${TARGET_DIR} && \
+        rm ${TARGET_DIR}/${DATASET_NAME}; \
+    else \
+        echo -e '\033[32m   >>Directory is not empty. Skipping unzip to avoid overwriting files.\033[0m'; \
+    fi \
+fi; \
 cd $DIR && python run_avocado.py --test_scene_dir '${TEST_SCENE_DIR}' && exec bash"
-
-
